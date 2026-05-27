40e4faf1897df73c32c82e8dae353447af460289
ulises-agr <gonzalezua@hitss.com>
2026-05-26 19:11:37 -0600

    remove effect

--- androidApp/src/main/kotlin/com/hellowordkmp/mobile/presenter/AppActivity.kt
+++ androidApp/src/main/kotlin/com/hellowordkmp/mobile/presenter/AppActivity.kt
@@ -7,7 +7,6 @@ package com.hellowordkmp.mobile.presenter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.SideEffect
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.hellowordkmp.mobile.presenter.navigation.AppNavigation
 import com.hellowordkmp.mobile.theme.AndroidSystemUiController
@@ -20,13 +19,11 @@ class AppActivity : ComponentActivity() {
         splashScreen.setKeepOnScreenCondition { false }
         setContent {
 //            val isDarkMode = isSystemInDarkTheme()
-            SideEffect {
-                AndroidSystemUiController(
-                    window = window,
-                    view = window.decorView,
-                ).apply {
-                    setSystemBarsColor(darkIcons = false)
-                }
+            AndroidSystemUiController(
+                window = window,
+                view = window.decorView,
+            ).apply {
+                setSystemBarsColor(darkIcons = false)
             }
             AppNavigation()
         }
